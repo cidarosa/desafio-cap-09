1d932b6df22cb0ce36c0c312fe175a55ef64932b
Aparecida Rosa <lucita.rosa@gmail.com>
2021-12-27 15:31:03 -0300

    clean project, deleted CardLoader,ProductInfoLoader, ProductDetailsLoadar, update Movie index NavLink

--- frontweb/src/pages/Movies/index.tsx
+++ frontweb/src/pages/Movies/index.tsx
@@ -1,43 +1,9 @@
-/*
-import { AxiosRequestConfig } from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Movie } from 'types/movie';
-import { SpringPage } from 'types/vendor/spring';
-import { requestBackend } from 'util/requests';*/
-
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './styles.css';
 
 const Movies = () => {
-/*
-    const [page, setPage] = useState<SpringPage<Movie>>();
-    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const params: AxiosRequestConfig = {
-            method: 'GET',
-            url: '/movies',
-            withCredentials: true,
-            params: {
-                page: 0,
-                size: 12,
-            },
-        };
-
-        setIsLoading(true);
-
-        requestBackend(params)
-            .then((response) => {
-                setPage(response.data);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            }
-            );
-    }, []);
-*/
     return (
         <div className="container my-4 movies-container">
 
@@ -46,23 +12,9 @@ const Movies = () => {
             </div>
 
             <div className="row movie-link">
-                <Link to="movies/1">Acessar /movies/1</Link>
-                <Link to="movies/2">Acessar /movies/2</Link>
-
-                {/*isLoading ? <CardLoader /> : (
-                    page?.content.map(movie => (
-                        <div className="col-sm-6 col-lg-4 col-xl-3" key={movie.id}>
-                            <Link to="movies/1">Acessar /movies/1</Link>
-                            <Link to="movies/2">Acessar /movies/2</Link>
-
-                        </div>
-                    )
-                    ))*/}
+                <NavLink to="/movies/1">Acessar /movies/1</NavLink>
+                <NavLink to="movies/2">Acessar /movies/2</NavLink>
             </div>
-            {/*
-            <div className="row">
-                <p>Criar Pagination</p>
-            </div>*/}
         </div>
     );
 };
